ac0743a450d3b7b022ca8a9a78c1f4e2d502d360
Seokchang-K <sckang1206@gmail.com>
2022-08-15 13:58:15 +0900

    20220815 solve

--- 6088.py
+++ 6088.py
@@ -1,7 +1,12 @@
 a, d, n = map(int, input().split())
 
-for i in range(a, n+1):
-    i = i + d
-    if i == n:
-        print(i)
+# for _ in range(n - 1):
+#     a += d
 
+# print(a)
+
+# 또는
+
+print(a+(n-1)*d)
+
+# 등차수열의 기본공식 a+(n-1)d
---
+++ 6089.py
@@ -0,0 +1,11 @@
+a, r, n = map(int,input().split())
+
+# for _ in range(n-1):
+#     a = a * r
+
+# print(a)
+
+# OR
+
+print(a*(r**(n-1)))
+# 이쪽은 등비 수열의 기본 공식이다.
